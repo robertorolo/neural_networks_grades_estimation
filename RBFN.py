ed7334c1f8235df93d88678a8217b3da67bcee3e
Roberto Rolo <robertorolo@gmail.com>
2019-08-01 23:06:23 -0300

    added sigma vectors

--- RBFN.py
+++ RBFN.py
@@ -3,6 +3,15 @@ from scipy.spatial.distance import cdist
 import plotly.offline as pyo
 import plotly.graph_objs as go
 from sklearn.cluster import KMeans
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import NearestNeighbors
+
+def train_test_sets_builder(df, x, y, z, var, test_size=0.3):
+
+    X = df[[x,y,z]].values
+    y = df[var].values
+
+    return train_test_split(X, y, test_size=0.33, random_state=42)
 
 def cluster_centers_evaluation(coordinates, max_num_clusters):
         
@@ -43,11 +52,19 @@ class RBFN:
 
             clusters_dist_mat = cdist(cluster_centers, cluster_centers)
             max_dist = np.max(clusters_dist_mat)
-            sigma = max_dist/np.sqrt(2*len(cluster_centers))
-            self.sigma = sigma
+            sigma = 1/(2*max_dist/(np.sqrt(2*len(cluster_centers))))**2
+            self.sigma = np.array([sigma] * len(cluster_centers)).T
 
         self.cluster_centers = cluster_centers
-        self.sigma = sigma
+        self.sigma = np.array([sigma] * len(cluster_centers))
+
+    def knn_sigma_definition(self, neighbors_number):
+        
+        knn = NearestNeighbors(n_neighbors=neighbors_number+1)
+        knn.fit(self.cluster_centers)
+        distances = knn.kneighbors(self.cluster_centers)[0]
+        #self.sigma = np.sqrt((np.sum(distances**2,axis=1))/neighbors_number)
+        self.sigma = 1/(2*np.max(distances,axis=1)/np.sqrt(2*neighbors_number))**2
 
     def gaussian_kernel(self, dist):
 
